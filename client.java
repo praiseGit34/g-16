565db07864a5d5f3e4e11167f6354f1a1674eb84
praiseGit34 <162679130+praiseGit34@users.noreply.github.com>
2024-06-06 16:58:14 +0300

    Update client.java

--- client.java
+++ client.java
@@ -1,5 +1,6 @@
 public class client{
     public static void main(String[] args) {
-        System.out.println("hello! world")
+        System.out.println("hello! world");
+            System.out.printLn("g 16");
     }
 }
